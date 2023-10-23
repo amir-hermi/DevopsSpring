8a01917af453fafb817d9f0434af9fe616ff7d96
unknown <amirhermi02@gmail.com>
2023-10-23 17:36:14 +0100

    Adding mock test for stock

--- src/test/java/tn/esprit/devops_project/DevOps_ProjectSpringBootApplicationTest.java
+++ src/test/java/tn/esprit/devops_project/DevOps_ProjectSpringBootApplicationTest.java
@@ -1,11 +1,14 @@
 package tn.esprit.devops_project;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class DevOps_ProjectSpringBootApplicationTest {
 
+    @Test
     void contextLoads() {
     }
 }
