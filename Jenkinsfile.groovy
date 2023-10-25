61820e01cbfb4f240b360cac400ec859cc580ca3
unknown <amirhermi02@gmail.com>
2023-10-25 21:35:25 +0100

    Adding Dockerfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -24,6 +24,42 @@ pipeline {
                 }
             }
         }
+        pipeline {
+    agent any
+    stages {
+        stage('Pulling..') {
+            steps {
+                script {
+                    git branch: 'master',
+                    credentialsId: 'spring-access-token',
+                    url: 'https://github.com/amir-hermi/DevopsSpring.git'
+                }
+            }
+        }
+        stage('Maven clean') {
+            steps {
+                script {
+                    sh 'mvn clean'
+                }
+            }
+        }
+        stage('Maven Compile') {
+            steps {
+                script {
+                    sh 'mvn compile'
+                }
+            }
+        }
+        stage('SonarQube Analysis') {
+            def mvn = tool 'Default Maven';
+            withSonarQubeEnv() {
+            sh "${mvn}/bin/mvn clean verify sonar:sonar -Dsonar.projectKey=sonarSpring -Dsonar.projectName='sonarSpring'"
+    }
+  }
+    }
+}
+
+
     }
 }
 
