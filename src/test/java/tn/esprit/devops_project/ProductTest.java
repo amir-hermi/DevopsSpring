0af44a60cbddd9bef48ac78bc8f5a27df0347461
RaniaChafai <rania.chafai@esprit.tn>
2023-10-28 03:27:06 +0100

    pom

--- src/test/java/tn/esprit/devops_project/ProductTest.java
+++ src/test/java/tn/esprit/devops_project/ProductTest.java
@@ -1,42 +1,69 @@
 package tn.esprit.devops_project;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import tn.esprit.devops_project.entities.Product;
+import tn.esprit.devops_project.entities.ProductCategory;
 import tn.esprit.devops_project.repositories.ProductRepository;
 import tn.esprit.devops_project.repositories.StockRepository;
 import tn.esprit.devops_project.services.ProductServiceImpl;
+import tn.esprit.devops_project.services.Iservices.IProductService;
+import tn.esprit.devops_project.entities.Stock;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-class ProductTest {
-    @MockBean
+public class ProductTest {
+
+    private IProductService productService;
+
+    @Mock
     private ProductRepository productRepository;
-    @MockBean
+
+    @Mock
     private StockRepository stockRepository;
 
-   @Autowired
-    private ProductServiceImpl productServiceImpl;
-
-    @Test
-    void testRetrieveAllProducts() {
-        List<Product> productList = new ArrayList<>();
-        when(productRepository.findAll()).thenReturn(productList);
-        List<Product> actualRetrieveAllProductResult = productServiceImpl.retreiveAllProduct();
-        assertSame(productList, actualRetrieveAllProductResult);
-        assertTrue(actualRetrieveAllProductResult.isEmpty());
-        verify(productRepository).findAll();
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        productService = new ProductServiceImpl(productRepository, stockRepository);
     }
 
     @Test
-    void testDeleteProduct() {
-        doNothing().when(productRepository).deleteById(any(Long.class));
-        productServiceImpl.deleteProduct(123L);
-        verify(productRepository).deleteById(any(Long.class));
+    public void testAddProduct() {
+        Stock stock = new Stock();
+        stock.setIdStock(1L);
+        Product product = new Product();
+        product.setIdProduct(1L);
+        product.setStock(stock);
+
+        when(stockRepository.findById(1L)).thenReturn(Optional.of(stock));
+        when(productRepository.save(Mockito.any(Product.class))).thenReturn(product);
+
+        Product addedProduct = productService.addProduct(product, 1L);
+
+        assertEquals(product, addedProduct);
+        verify(stockRepository, times(1)).findById(1L);
+        verify(productRepository, times(1)).save(Mockito.any(Product.class));
+    }
+
+    @Test
+    public void testRetrieveProductByCategory() {
+        ProductCategory category = ProductCategory.ELECTRONICS;
+        List<Product> productList = new ArrayList<>();
+
+        when(productRepository.findByCategory(category)).thenReturn(productList);
+
+        List<Product> retrievedProducts = productService.retrieveProductByCategory(category);
+
+        assertEquals(productList, retrievedProducts);
+        verify(productRepository, times(1)).findByCategory(category);
     }
 }
