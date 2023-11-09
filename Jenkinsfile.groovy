99196276dad7ca9615377bb22222ef2ba2d656f7
bilelT9 <62221536+bilelT9@users.noreply.github.com>
2023-11-09 22:29:43 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -40,6 +40,17 @@ pipeline {
                 sh 'mvn deploy -Dmaven.test.skip=true'
             }
         }
+
+stage('GRAFANA') {
+    steps {
+        script {
+            def grafanaUrl = 'http://192.168.0.18:3000'
+            def username = 'admin'
+            def password = '213JMT5123T99'
+            sh "curl -u $username:$password -X POST $grafanaUrl/api/dashboards/db -d @grafana_dashboard.json"
+        }
+    }
+}
     }
 }
 
