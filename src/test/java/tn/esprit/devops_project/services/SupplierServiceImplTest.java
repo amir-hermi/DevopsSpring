0a3d31056e5e911c98c34c73819352770b002a73
ahmed hbaieb <hbaieb.ahmed@hotmail.fr>
2023-11-09 10:38:54 +0100

    theni commit ya rojla

--- src/test/java/tn/esprit/devops_project/services/SupplierServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/SupplierServiceImplTest.java
@@ -1,12 +1,15 @@
 package tn.esprit.devops_project.services;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import tn.esprit.devops_project.entities.ActivitySector;
 import tn.esprit.devops_project.entities.Invoice;
 import tn.esprit.devops_project.entities.Supplier;
@@ -19,6 +22,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,25 +30,13 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class SupplierServiceImplTest {
 
-
-    Long idSupplier;
-    String code;
-    String label;
-    @Enumerated(EnumType.STRING)
-    SupplierCategory supplierCategory;
-    @OneToMany(mappedBy="supplier")
-    @JsonIgnore
-    Set<Invoice> invoices;
-    @ManyToMany
-    private Set<ActivitySector> activitySectors;
-
-
-
-
     @InjectMocks
     SupplierServiceImpl isupplierService;
     @Mock
     SupplierRepository supplierRepository;
+
+    @Autowired
+    SupplierServiceImpl supplierService;
 
     Supplier supp1 = new Supplier(1L , "code1" ,"label" , SupplierCategory.ORDINAIRE , null , null  ) ;
     List<Supplier> listsupplier = new ArrayList<Supplier>() {
@@ -55,32 +47,44 @@ class SupplierServiceImplTest {
         }
     };
 
-
-//
-//    @Test
-//    void retrieveAllSuppliers() {
-//    }
+    @Test
+    void retrieveAllSuppliers() {
+        Mockito.when(supplierRepository.findAll()).thenReturn(listsupplier);
+        List<Supplier> suppliersRetournes = isupplierService.retrieveAllSuppliers();
+        assertEquals(2, suppliersRetournes.size());
+        assertEquals(2L, suppliersRetournes.get(0).getIdSupplier());
+        assertEquals(3L, suppliersRetournes.get(1).getIdSupplier());
+    }
 
     @Test
     void addSupplier() {
-
         Mockito.when(supplierRepository.save(supp1)).thenReturn(supp1);
-
         Supplier supplierAjoute = isupplierService.addSupplier(supp1);
-
         assertEquals("code1", supplierAjoute.getCode());
+    }
+
+    @Test
+    void retrieveSupplier() {
+        Mockito.when(supplierRepository.findById(1L)).thenReturn(Optional.of(supp1));
+        Supplier suppp = isupplierService.retrieveSupplier(1L);
+        assertNotNull(suppp);
 
     }
 
-//    @Test
-//    void updateSupplier() {
-//    }
-//
-//    @Test
-//    void deleteSupplier() {
-//    }
-//
-//    @Test
-//    void retrieveSupplier() {
-//    }
+    @Test
+    void updateSupplier() {
+        Supplier updatedSupplier = new Supplier(1L , "code1" ,"label" , SupplierCategory.ORDINAIRE , null , null  ) ;
+        isupplierService.updateSupplier(updatedSupplier);
+        Mockito.verify(supplierRepository).save(updatedSupplier);
+    }
+
+    @Test
+    void deleteSupplier() {
+        isupplierService.deleteSupplier(1L);
+        Mockito.verify(supplierRepository).deleteById(1L);
+
+    }
+
+
+
 }
